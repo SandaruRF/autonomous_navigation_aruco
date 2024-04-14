b3b51fff2c8907dc7e393a883778e1128c608f09
sandaru <sandarufernando2002@gmail.com>
2024-04-14 12:11:47 +0530

    final code

--- autonomous_navigation/autonomous_navigation.ino
+++ autonomous_navigation/autonomous_navigation.ino
@@ -1,3 +1,25 @@
+#include <Dictionary.h>
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+Dictionary<String, int>
+directions = {
+  {"East", 90},
+  {"South-East", 135},
+  {"South", 180},
+  {"South-West", 225},
+  {"West", 270},
+  {"North-West", 315},
+  {"North", 0},
+  {"North-East", 45}
+}
+
+void Move(int cur_ang, int prev_ang);
+
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
@@ -13,5 +35,12 @@ void loop() {
     }else {
       digitalWrite(13, LOW);
     }
+  }
+}
+
+void Move(int cur_ang, int prev_ang){
+  diff = cur_ang - prev_ang;
+  if (diff == 0){
+    Run(FORWARD, STRAIGHT
   }
 }
